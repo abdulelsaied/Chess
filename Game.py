2a97800827e7a4de85a5b506c997dda0e2dc6b06
Abdul El-Saied <abdule@Abduls-MacBook-Air.local>
2021-12-16 13:58:57 -0800

    first commit

---
+++ Game.py
@@ -0,0 +1,233 @@
+import re
+
+direction_offsets = [8, -8, 1, -1, 7, -7, 9, -9]
+squares_to_edge = {}
+
+
+"""
+Returns an array containing distances to edge for each square
+squares_to_edge[i] = [n, s, e, w, sw, ne, se, nw]
+"""
+def precompute_data():
+    for i in range(8):
+        for j in range(8):
+            board_num = i * 8 + j
+            north = i
+            south = 7 - i
+            east = 7 - j
+            west = j
+            south_west = min(south, west)
+            north_east = min(north, east)
+            south_east = min(south, east)
+            north_west = min(north, west)
+            squares_to_edge[board_num] = [north, south, east, west, south_west, north_east, south_east, north_west]
+    return squares_to_edge
+
+def fen_to_array(fen_string):
+    #assert re.match('\s*^(((?:[rnbqkpRNBQKP1-8]+\/){7})[rnbqkpRNBQKP1-8]+)\s([b|w])\s([K|Q|k|q]{1,4})\s(-|[a-h][1-8])\s(\d+\s\d+)$', fen_string)
+    fields = fen_string.split(" ")
+    assert len(fields) == 6
+    board_info = fields[0]
+    color = fields[1]
+    castling = fields[2]
+    enpassant = fields[3]
+    halfmove = fields[4]
+    fullmove = fields[5]
+
+    # should import constants once its a class and use the value here
+    board = [["--" for i in range(8)] for j in range(8)]
+    current_rank = 0
+    current_file = 0
+
+    for char in board_info:
+        piece = '--'
+        if (char == '/'):
+            current_rank += 1
+            current_file = 0
+        else:
+            if (char.isdigit()): 
+                current_file += int(char)
+            else:
+                if (char.isupper()): # white piece
+                    board[current_rank][current_file] = 'w' + char
+                else:
+                    board[current_rank][current_file] = 'b' + char.upper()
+                current_file += 1
+
+    white_to_move = True
+    if color == 'b':
+        white_to_move = False
+    
+    return (board, white_to_move)
+    
+class GameState():
+
+    def __init__(self, fen_string = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
+        precompute_data()
+        self.board, self.whites_turn = fen_to_array(fen_string)
+        self.move_log = []
+        self.move_set = []
+
+
+    def make_move(self, move):
+        ### move must start with current players piece, end on empty square or an opponents piece
+        first_piece = move.piece_moved
+        second_piece = move.piece_captured
+        if (('w' in first_piece and self.whites_turn) or ('b' in first_piece and not self.whites_turn)) and (('w' in second_piece and not self.whites_turn) or ('b' in second_piece and self.whites_turn) or second_piece == '--'):
+            print("first_piece ", first_piece)
+            print("second_piece ", second_piece)
+            ### validate move made here
+            legal_moves = self.generate_pseudo_legal_moves()
+            print("Legal moves: ", legal_moves)
+            self.board[move.start_row][move.start_col] = '--'
+            self.board[move.end_row][move.end_col] = move.piece_moved
+            self.move_log.append(move)
+            self.whites_turn = not self.whites_turn
+            print("move_log: ", self.move_log)
+
+    def undo_move(self):
+        if self.move_log:
+            move = self.move_log.pop()
+            self.board[move.start_row][move.start_col] = move.piece_moved
+            self.board[move.end_row][move.end_col] = move.piece_captured
+            self.whites_turn = not self.whites_turn
+
+
+    def generate_pseudo_legal_moves(self):
+        possible_moves = []
+        for i in range(8):
+            for j in range(8):
+                piece = self.board[i][j]
+                board_num = i * 8 + j
+                print(board_num)
+                if (self.is_friendly_piece(board_num)):
+                    if ('P' in piece): # this is a pawn
+                        print("pawn")
+                        possible_moves += (self.generate_pawn_moves(board_num))
+                    elif ('Q' in piece):
+                        print("queen")
+                        possible_moves += (self.generate_sliding_moves(board_num, 'Q'))
+                    elif ('K' in piece):
+                        print("king")
+                        possible_moves += (self.generate_king_moves(board_num))
+                    elif ('B' in piece):
+                        print("bishop")
+                        possible_moves += (self.generate_sliding_moves(board_num, 'B'))
+                    elif ('R' in piece):
+                        print("rook")
+                        possible_moves += (self.generate_sliding_moves(board_num, 'R'))
+                    elif ('N' in piece):
+                        print("knight")
+                        possible_moves += (self.generate_knight_moves(board_num))
+        return possible_moves
+
+    # boardnum is a number from 0 to 63, i * 8 + j
+    def generate_sliding_moves(self, board_num, piece_type):
+        print('got here')
+        possible_moves = []
+        start_index = 4 if piece_type == 'B' else 0
+        end_index = 4 if piece_type == 'R' else 8
+        piece_on_current_square = self.get_square_at_board_index(board_num)
+        for direction_index in range(start_index, end_index): ## end_index + 1?
+            for n in range(squares_to_edge[board_num][direction_index]):
+                target_square = board_num + direction_offsets[direction_index] * (n + 1)
+                print("target_square ", target_square)
+                print("n ", n)
+                print("direction_index ", direction_index)
+                if (self.is_friendly_piece(target_square)):
+                    break
+                piece_on_target_square = self.get_square_at_board_index(target_square)
+                possible_moves.append(Move(piece_on_current_square, piece_on_target_square, self.board))
+                print("Got here")
+                if (not self.is_friendly_piece(target_square)):
+                    break
+        return possible_moves
+
+    def generate_pawn_moves(self, board_num):
+        print('got here')
+        possible_moves = []
+        current_square = self.get_square_at_board_index(board_num)
+        if (self.whites_turn):
+            if (self.get_piece_at_board_index(board_num - 7) == '--'):
+                possible_moves.append(Move(current_square, self.get_square_at_board_index(board_num - 7), self.board))
+            # if (self.get_piece_at_board_index()):
+            if (self.get_piece_at_board_index(board_num - 8) == '--'):
+                possible_moves.append(Move(current_square, self.get_square_at_board_index(board_num - 8), self.board))
+            if (self.get_piece_at_board_index(board_num - 8) == '--'):
+                possible_moves.append(Move(current_square, self.get_square_at_board_index(board_num - 8), self.board))
+        return possible_moves
+    def generate_king_moves(board_num):
+        return []
+    def generate_knight_moves(board_num):
+        return []
+    def generate_legal_moves(self):
+        pass
+
+    def get_image_at_coordinates(self, xcoor, ycoor):
+        return self.board[xcoor][ycoor]
+
+    
+    '''
+    Returns the piece at a specific coordinate
+    input: index ([0, 0])
+    output: piece ('wP')
+    '''
+    def get_piece_at_index(self, index):
+        return self.board[index[0]][index[1]]
+
+    '''
+    Returns the piece at a specific board index
+    input: board_num (54)
+    output: piece ('wP')
+    '''
+    def get_piece_at_board_index(self, board_num):
+        row_value = board_num // 8
+        col_value = board_num % 8
+        return self.board[row_value][col_value] 
+
+    '''
+    Returns the square at a specific board index
+    input: board_num (0)
+    output: (0, 0)
+    '''
+    def get_square_at_board_index(self, board_num):
+        row_value = board_num // 8
+        col_value = board_num % 8
+        return (row_value, col_value)     
+
+    def is_friendly_piece(self, board_num):
+        piece = self.get_piece_at_board_index(board_num)
+        if (('w' in piece and self.whites_turn) or ('b' in piece and not self.whites_turn)):
+            return True
+        return False
+
+    def print_turn(self):
+        if (self.whites_turn):
+            print('Whites turn')
+        else:
+            print('Blacks turn')
+
+
+class Move():
+
+    ranks_to_rows = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
+    rows_to_ranks = {i: j for j, i in ranks_to_rows.items()}
+    files_to_cols = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+    cols_to_files = {i: j for j, i in files_to_cols.items()}
+    
+    def __init__(self, start_square, end_square, board):
+        assert start_square 
+        assert end_square
+        self.start_row = start_square[0]
+        self.start_col = start_square[1]
+        self.end_row = end_square[0]
+        self.end_col = end_square[1]
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+
+
+    def get_chess_notation(self):
+        return self.get_rank_file(self.start_row, self.start_col) + self.get_rank_file(self.end_row, self.end_col)
+
+    def get_rank_file(self, row, col):
+        return self.cols_to_files[col] + self.rows_to_ranks[row]
---
+++ Gui.py
@@ -0,0 +1,157 @@
+import pygame as p 
+import Game
+
+#constants
+WIDTH = HEIGHT = 512
+ROWS = COLS = 8
+SQUARE_LENGTH = HEIGHT // ROWS 
+FPS = 30
+IMAGES = {}
+PIECES = ['wP', 'bP', 'wR', 'bR', 'wN', 'bN', 'wB', 'bB', 'wK', 'bK', 'wQ', 'bQ']
+COLORS = ['#ebecd0', '#779556']
+
+WIN = p.display.set_mode((WIDTH, HEIGHT))
+p.display.set_caption('Chess Project')
+
+def load_images():
+    for piece in PIECES:
+        IMAGES[piece] = p.transform.scale(p.image.load('images/' + piece + "-highres.png"), (SQUARE_LENGTH, SQUARE_LENGTH))
+
+"""
+Returns a tuple desribing the coordinates of a square clicked.
+"""
+def get_current_square(mouse):
+    return (mouse.get_pos()[1] // SQUARE_LENGTH, mouse.get_pos()[0] // SQUARE_LENGTH)
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    gamestate = Game.GameState()
+    load_images()
+    run = True
+    selected_square = ()
+    clickable = False
+    draggable = False
+    drag_now = False
+    image_to_drag = ""
+
+
+
+
+    '''
+    After each mouseclick, coordinates are captured in a tuple current_square
+    The piece on that square is square_string
+    This function should not handle any game logic, but call functions in game.py
+    DOWN: 
+    if clickable is true, there are 2 possibilities:
+        clicked on new square --> make the move between selected_square and recently clicked square
+        clicked on same square --> cancel clickable ability a
+    if clickable is false, then we are starting the drag and drop process, so set selected_square to clicked button and drag to true
+
+    MOTION: 
+    if drag is true, image should follow cursor
+
+    UP: 
+    if selected_square, there are __ possibilities:
+        letting go on same square, should set clickable to true and selected_square to same square
+        letting go on different square, make move between selected_square and current square and reset all variables.
+
+    '''
+    while run:
+        clock.tick(FPS)
+        mx, my = p.mouse.get_pos()
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                run = False
+
+            # sets square and piece corresponding to position of the mouse.
+            current_square = get_current_square(p.mouse)
+            current_piece = gamestate.get_piece_at_index(current_square)
+
+            # if mousedown:
+            # scenario 1: clicking a piece for the first time
+            # scenario 2: had previously clicked and released. clicking a new square
+            # scenario 3: had previously clicked and released. clicking the same square 
+            if event.type == p.MOUSEBUTTONDOWN:
+                if (('w' in current_piece and gamestate.whites_turn) or ('b' in current_piece and not gamestate.whites_turn)):
+                    if (clickable):
+                        if (current_square != selected_square):
+                            move = Game.Move(selected_square, current_square, gamestate.board)
+                            print(move.get_chess_notation())
+                            gamestate.make_move(move)
+                            gamestate.print_turn()
+                        selected_square = ()
+                        clickable = False
+                        draggable = False
+                    else:
+                        selected_square = current_square 
+                        draggable = True
+                        drag_now = True
+                        xcoor = selected_square[0]
+                        ycoor = selected_square[1]
+                    image_key = gamestate.get_image_at_coordinates(xcoor, ycoor)
+                    if image_key == '--':
+                        break
+                    image_to_drag = IMAGES[image_key]
+
+            elif event.type == p.MOUSEBUTTONUP:
+                drag_now = False
+                if (not clickable and not draggable):
+                    break
+                if (current_square == selected_square):
+                    clickable = True
+                    draggable = False
+                else:
+                    print(selected_square)
+                    move = Game.Move(selected_square, current_square, gamestate.board)
+                    print(move.get_chess_notation())
+                    gamestate.make_move(move)
+                    gamestate.print_turn()
+                    selected_square = ()
+                    clickable = False
+                    draggable = False
+
+            elif event.type == p.MOUSEMOTION:
+                # piece we want to drag is at selected_square
+                # how to get image at that coordinate
+                if selected_square and draggable:
+                    drag_now = True
+                    xcoor = selected_square[0]
+                    ycoor = selected_square[1]
+                    image_to_drag = IMAGES[gamestate.get_image_at_coordinates(xcoor, ycoor)]
+
+
+            elif event.type == p.KEYDOWN:
+                if (event.key) == p.K_z:
+                    print('pressed undo')
+                    gamestate.undo_move()
+        
+        draw_gamestate(screen, gamestate)
+        if p.mouse.get_pressed()[0]:
+            draw_dragged_piece(screen, mx, my, image_to_drag, drag_now)
+        p.display.flip()
+
+def draw_gamestate(screen, gamestate):
+    draw_board(screen)
+    draw_pieces(screen, gamestate.board)
+
+def draw_board(screen):
+    for i in range(ROWS):
+        for j in range(COLS):
+            p.draw.rect(screen, COLORS[(i + j) % 2], p.Rect(j * SQUARE_LENGTH, i * SQUARE_LENGTH, SQUARE_LENGTH, SQUARE_LENGTH))
+
+def draw_pieces(screen, board):
+    for i in range(ROWS):
+        for j in range(COLS):
+            piece = board[i][j]
+            if (piece != '--'):
+                screen.blit(IMAGES[piece], p.Rect(j * SQUARE_LENGTH, i * SQUARE_LENGTH, SQUARE_LENGTH, SQUARE_LENGTH))
+
+def draw_dragged_piece(screen, mx, my, image, drag_now):
+    if (drag_now):
+        screen.blit(image, (mx - 25, my - 25))
+
+if __name__ == '__main__':
+    main()
